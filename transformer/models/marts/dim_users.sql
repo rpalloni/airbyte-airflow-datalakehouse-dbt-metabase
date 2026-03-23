4ab631290ed65ff406e1f1993ab779cfdde6025d
rpalloni <rpdev20@gmail.com>
2026-03-23 13:40:13 +0100

    fix source

--- transformer/models/marts/dim_users.sql
+++ transformer/models/marts/dim_users.sql
@@ -4,8 +4,8 @@ SELECT
     user_id
     , user_name
     , CASE 
-        WHEN user_name LIKE 'M%' THEN 'Marketplace'
-        WHEN user_name LIKE 'U%' THEN 'Store'
+        WHEN user_id LIKE 'M%' THEN 'Marketplace'
+        WHEN user_id LIKE 'U%' THEN 'Store'
         ELSE 'Unknown'
     END                     AS source
     , created_ts
